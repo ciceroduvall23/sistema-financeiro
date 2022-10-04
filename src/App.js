a81906eddde929de1b3342100bc3894ac8042d04
ciceroduvall23 <74693314+ciceroduvall23@users.noreply.github.com>
2022-10-04 00:12:08 -0300

    add file

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Header from "./components/header";
+import Resume from "./components/resume"
+import Global from "./styles/global"
+
+const App = () => {
+return (
+  <>
+ <Header/>
+ <Resume />
+  <Global />
+  </>
+);
+};
+export default App;
