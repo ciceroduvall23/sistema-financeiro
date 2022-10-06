6d09067460293483f69547ae094220ef7008766e
ciceroduvall23 <74693314+ciceroduvall23@users.noreply.github.com>
2022-10-05 23:56:42 -0300

    update

--- src/components/form/index.js
+++ src/components/form/index.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-
+import * as C from "./style"
 
 const Form = () => {
     const [desc,setDesc] = useState("");
@@ -7,15 +7,49 @@ const Form = () => {
     const [IsExpense, setExpense] = useState(false);
 
     const handleSave = () =>{
-        if(!desc || ! amount){
+        if(!desc || !amount){
             alert("Informe a descrição e o valor");
             return;
          } else if (amount < 1) { 
                 alert("O valor tem que ser positivo");
+                return;
          }  
     };
+
     return (
-       <div>Form</div>
+       <>
+
+       <C.Container>
+        <C.InputContent>
+            <C.Label>Descrição</C.Label>
+           <C.Input value={desc} onChance={(e) => setDesc(e.target.value)} />
+        </C.InputContent>
+        <C.InputContent>
+        <C.Label>Valor</C.Label>
+           <C.Input 
+            value={amount}
+             type="number" 
+             onChance={(e) => setAmount(e.target.value)} />
+        </C.InputContent>
+        <C.RadioGroup>
+        <C.Input 
+             type="radio" 
+             id="rIncome"
+             defaultCkecked
+             name="group1"
+             onChance={() => setExpense(!IsExpense)} />
+
+             <C.Label htmlFor ="rIncome">Entrada</C.Label>
+             <C.Input 
+             type="radio" 
+             id="rExpenses"
+             name="group1"
+             onChance={() => setExpense(!IsExpense)} />
+               <C.Label htmlFor ="rExpenses">Saída</C.Label>
+        </C.RadioGroup>
+        <C.Button onClick={handleSave}>ADICIONAR</C.Button>
+       </C.Container>
+       </>
     );
 };
 
--- src/components/form/style.js
+++ src/components/form/style.js
@@ -1,12 +1,12 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-   max-width:1120px;
-   margin:20px;
+   max-width: 1120px;
+   margin: 20px auto;
    width:98%;
    background-color:#fff;
    box-shadow:0px 0px 5px #ccc;
-   border-radius:5px;
+   border-radius: 5px;
    display:flex;
    justify-content:space-round;
    padding:15px 0px;
@@ -17,11 +17,13 @@ export const Container = styled.div`
 }
 
 `;
-export const ImputContent = styled.div`
+export const Label = styled.label``;
+
+export const InputContent = styled.div`
 display:flex;
-flex-direction:collumn;
+flex-direction:column;
 `
-export const Input = styled.div`
+export const Input = styled.input`
  outline:none;
  border-radius:5px;
  padding:5px 10px;
@@ -35,12 +37,13 @@ export const RadioGroup = styled.div`
      
    input{
     margin-left:20px;
+    margin-right:20px;
     accent-color:black;
     margin-top:0;
    }
    `;
 
-   export const button = styled.button`
+   export const Button = styled.button`
      padding:5px 10px;
      border:none;
      border-radius:5px;
